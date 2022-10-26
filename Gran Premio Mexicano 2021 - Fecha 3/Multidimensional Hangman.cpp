12633f99d52cc1897c003801ddf5d02e1427ae79
EmilioPFematt <supermageblaziken@gmail.com>
2022-10-26 15:04:30 -0500

    Multidimensional Hangman

--- Gran Premio Mexicano 2021 - Fecha 3/Multidimensional Hangman.cpp
+++ Gran Premio Mexicano 2021 - Fecha 3/Multidimensional Hangman.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -11,16 +10,29 @@ int main(){
     int N, K; cin>>N>>K;
     for(int i = 0; i<N; i++){
         string s; cin>>s;
-        for(int i = 0; i<K; i++){
-            if(s[i] == '*'){
-                for(int i = 0; i<27; i++){
-                    s[i] = ('a'+i);
+        for(int j = 0; j<K; j++){
+            if(s[j] == '*'){
+                for(int k = 0; k<27; k++){
+                    s[j] = ('a'+k);
                     p.push_back(s);
                 }
+                break;
             }
-            break;
         }
     }
-    sort(p.begin(), p.end();
+    sort(p.begin(), p.end());
+    int ans = 0, c = 1;
+    string s_ans = "";
+    for(int i = 1; i<p.size(); i++){
+        if(p[i-1] != p[i]){
+            if(c > ans){
+                s_ans = p[i-1];
+                ans = c;
+            }
+            c = 0;
+        }
+        c++;
+    }
+    cout<<s_ans<<" "<<ans;
     return 0;
 }
